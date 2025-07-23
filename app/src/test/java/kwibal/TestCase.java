2852d4f7d77fe8c08c71ed5d402dd69f6ab55b55
Jafar Sherif <jafarsherif27@gmail.com>
2025-07-23 23:32:54 +0530

    Update TestCase.java

--- app/src/test/java/kwibal/TestCase.java
+++ app/src/test/java/kwibal/TestCase.java
@@ -4,19 +4,13 @@ import java.time.Duration;
 
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-// import org.testng.annotations.AfterClass;
-// import org.testng.annotations.AfterTest;
-// import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestCase {
